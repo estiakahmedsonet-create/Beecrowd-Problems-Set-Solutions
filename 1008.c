afc33da361f3031f8c8969612494814434f2c673
Estiak Ahmed Sonet <estiakahmedsonet@gmail.com>
2026-04-10 20:23:29 +0600

    Add files via upload

---
+++ 1008.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+
+   int num,wh;
+   float amount,salary;
+   scanf("%d%d%f",&num,&wh,&amount);
+   salary=wh*amount;
+   printf("NUMBER = %d\nSALARY = U$ %.2f\n",num,salary);
+
+
+
+    return 0;
+
+
+
+}
+
